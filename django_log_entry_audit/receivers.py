48baaa0e471570b19e1fba0ff868c464ef2d2a41
Daniel Đukić <daniel@dukic.dev>
2021-07-08 16:24:19 +0200

    Add bulk_update receiver

--- django_log_entry_audit/receivers.py
+++ django_log_entry_audit/receivers.py
@@ -145,3 +145,41 @@ try:
 
 except KeyError:
     pass
+
+
+try:
+
+    @receiver(AUDIT_SIGNALS["bulk_update"])
+    def bulk_update_log_entries(sender, **kwargs):
+        objs = kwargs["objs"]
+        user = kwargs["user"]
+
+        if len(objs) == 1:
+            obj = objs[0]
+            LogEntry.objects.create(
+                object_id=obj.pk,
+                app_label=obj._meta.app_label,
+                model_name=obj._meta.model_name,
+                fields=_get_object_serialized_fields(obj),
+                user=user,
+                status=StatusEnum.UPDATED,
+            )
+        else:
+            log_entries = []
+            for obj in objs:
+                log_entries.append(
+                    LogEntry(
+                        object_id=obj.pk,
+                        app_label=obj._meta.app_label,
+                        model_name=obj._meta.model_name,
+                        fields=_get_object_serialized_fields(obj),
+                        user=user,
+                        status=StatusEnum.UPDATED,
+                    )
+                )
+
+            LogEntry.objects.bulk_create(log_entries)
+
+
+except KeyError:
+    pass
